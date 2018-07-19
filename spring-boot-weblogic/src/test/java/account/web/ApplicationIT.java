eb6edcb5bf0c245c225546e20c9e01cdc0d7d067
CREDSYSTEM\cleiton.ramalho <cleiton.ramalho@credsystem.com.br>
2018-07-19 12:48:05 -0300

    Iniciando Spring com weblogic
    Signed-off-by: CREDSYSTEM\cleiton.ramalho <cleiton.ramalho@credsystem.com.br>

---
+++ spring-boot-weblogic/src/test/java/account/web/ApplicationIT.java
@@ -0,0 +1,23 @@
+package account.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+/**
+ * Basic integration tests for demo application.
+ *
+ * @author DISID
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ApplicationIT {
+
+
+	  @Test
+	  public void httpShowAccount() throws Exception {
+
+	  }
+
+
+}
